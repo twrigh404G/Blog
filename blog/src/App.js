d05e91c67d6be8df2fd320125dd9a39983cd693a
Terrell <wrightterrell049@gmail.com>
2022-07-05 16:47:28 -0400

    init clientside

---
+++ blog/src/App.js
@@ -0,0 +1,20 @@
+import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return(
+    <div>
+      hello
+    </div>      
+    );
+  }
+}
+
+export default App;
--- blog/src/index.js
+++ blog/src/index.js
@@ -1,20 +1,14 @@
-'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-module.exports = {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/*{ strapi }*/) {},
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/*{ strapi }*/) {},
-};
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
