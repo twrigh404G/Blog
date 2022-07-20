06e17348d2d3c6d3c90579227db6fe8bb22d107c
Terrell <wrightterrell049@gmail.com>
2022-07-20 16:46:45 -0400

    React Three Fiber init Hero

--- blog/src/Threehero.js
+++ blog/src/Threehero.js
@@ -2,8 +2,10 @@ import ReactDOM from 'react-dom';
 import { useState, useRef} from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import Box from '@mui/material/Box';
+import { Typography } from '@mui/material';
 
-const Threehero = () => {
+const Threehero = ( )=> {
+    
 
 
     return(
@@ -11,9 +13,14 @@ const Threehero = () => {
             width: 100,
             height: 400,
         }}>
+            <Typography variant="h1">Header</Typography>
             <Canvas>
-                <ambientLight />
-                <pointLight position={[10, 10, 10]} />
+                <ambientLight intensity={0.1} />
+                <directionalLight color="black" position={[3, 6, 5]} />
+                <mesh>
+                    <boxGeometry />
+                    <meshStandardMaterial />
+                </mesh>
             </Canvas>
         </Box>
         
